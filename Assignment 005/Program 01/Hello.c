8282388f53b331ea200a0a16581b060af02f4310
Siv Sagar <sivsagar29112006@gmail.com>
2025-09-03 04:31:25 +0000

    push

--- Assignment 005/Program 01/Hello.c
+++ Assignment 005/Program 01/Hello.c
@@ -5,4 +5,5 @@ i) ush - add item to stack (Print overflow if stack is full)
 ii) pop - remove item from stack
 iii) peek - see the top item of stack
 iv) view - gives the view of the stack
+Instruction:- 1. You may use a global integer for the stack.
 */
